8f171168952f3441f5348e985db9d1aa4db4c82b
Akhadjon Abdukhalilov <abduxalilovahadjon97@gmail.com>
2021-02-07 16:44:34 +0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Akhadjon Abdukhalilov on 2/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
